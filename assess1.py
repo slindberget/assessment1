35dc1cd5d0922a2f498cb18fb3e396ca32163f3b
slindberget <118910064+slindberget@users.noreply.github.com>
2022-12-06 11:30:16 +0100

    commit 4

--- assess1.py
+++ assess1.py
@@ -8,13 +8,26 @@ conn = psycopg2.connect(
     password="Ghznmqm7"
     )
 
-def read_contacts(C):
-    cur = C.cursor()
+def read_contacts(conn):
+    cur = conn.cursor()
     cur.execute("SELECT * FROM contacts;")
     rows = cur.fetchall()
     cur.close()
     return rows
 
+def add_contact(conn, first_name, last_name, title, organization):
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO contacts(first_name, last_name, title, organization) VALUES ('{first_name}', '{last_name}', '{title}', '{organization}');")
+    cur.close()
+
 while True: ## REPL - Read Execute Program Loop
-    cmd = input("Hello and welcome to the contacts list, available commands: \nadd - add a phone number\ndelete - delete a contact \nlist - list all phone numbers \nquit - quit the program\nsave -save withour quit\nhelp - help \nCommand: ").upper()
+    cmd = input("Hello and welcome to the contacts list, available commands: \nadd - add a contact\ndelete - delete a contact \nlist - list all contacts \nquit - quit the program\nsave -save withour quit\nhelp - help \nCommand: ").upper()
+    if cmd == "LIST":
+        print(read_contacts(conn))
+    elif cmd == "ADD":
+        first_name = input("  First name: ")
+        last_name = input("  Last name: ")
+        title = input("  title: ")
+        organization = input("Organization:  ")
+        add_contact(conn, first_name, last_name, title, organization)
 
