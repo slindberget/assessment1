30d388403c3fe5423cf21479f66ef99e5a158df6
slindberget <118910064+slindberget@users.noreply.github.com>
2022-12-06 10:58:27 +0100

    third

--- assess1.py
+++ assess1.py
@@ -14,3 +14,7 @@ def read_contacts(C):
     rows = cur.fetchall()
     cur.close()
     return rows
+
+while True: ## REPL - Read Execute Program Loop
+    cmd = input("Hello and welcome to the contacts list, available commands: \nadd - add a phone number\ndelete - delete a contact \nlist - list all phone numbers \nquit - quit the program\nsave -save withour quit\nhelp - help \nCommand: ").upper()
+
