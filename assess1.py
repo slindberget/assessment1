b407a68425dfd7c9dcab21054e4c9a4f8ecc6bf1
slindberget <118910064+slindberget@users.noreply.github.com>
2022-12-06 10:55:43 +0100

    Commit 2

--- assess1.py
+++ assess1.py
@@ -5,7 +5,12 @@ conn = psycopg2.connect(
     host="localhost",
     database="assessmentdb",
     user="postgres",
-    password="xxx"
+    password="Ghznmqm7"
     )
 
-    
+def read_contacts(C):
+    cur = C.cursor()
+    cur.execute("SELECT * FROM contacts;")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
